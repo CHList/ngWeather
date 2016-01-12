e563f050d46413fc64bf8f5de388e9e5dea9f9cc
hey@illicitr.us <hey@illicitr.us>
2016-01-12 14:15:16 +0500

    initial commit

---
+++ app.js
@@ -0,0 +1,32 @@
+var app = angular.module('weatherApp', []);
+
+app.controller('weatherController', function($scope,$http,$filter){
+	$scope.title = 'What\'s the Temperature';
+	$scope.isCity = false;
+
+	$scope.getWeather = function(){
+		$http({
+			method: 'GET',
+			url: 'http://api.openweathermap.org/data/2.5/weather?q='+ $scope.city +'&appid=2de143494c0b295cca9337e1e96b00e0&units=metric'
+		}).then(function successCallback(response) {
+			var data = response.data;
+			var temperature = data.main.temp;
+			var city = data.name;
+			var min = data.main.temp_min;
+			var max = data.main.temp_max;
+			if ($filter('lowercase')(city) == $filter('lowercase')($scope.city)) {
+				$scope.symbol = '℃';
+				$scope.temperature = temperature;
+				$scope.min = min;
+				$scope.max = max;
+				$scope.title = city + ' - ' + temperature + '℃';
+				$scope.isCity = true;
+			}else{
+				$scope.message = 'I am not familiar with that city, Sorry';
+				$scope.title = 'What\'s the Temperature'
+				$scope.isCity = false;
+			}
+		});
+	}
+
+});
