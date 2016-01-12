a34a28848d0156505840e51d85f1408bcee23e78
Uzair Mehmood <hey@illicitr.us>
2016-01-12 15:36:31 +0500

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 var app = angular.module('weatherApp', []);
 app.constant("apiKey", "2de143494c0b295cca9337e1e96b00e0")
-app.controller('weatherController', function($scope,$http,$filter){
+app.controller('weatherController', function($scope,$http,$filter, apiKey){
 	$scope.title = 'What\'s the Temperature';
 	$scope.isCity = false;
 
